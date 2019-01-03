02efa49428415f19dc62d121daf9f83497674ba7
Carnival2003 <45796669+Carnival2003@users.noreply.github.com>
2019-01-03 23:44:46 +0300

    Add files via upload

---
+++ Mission3-1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Введите строку: ");
+		String str = in.nextLine();
+		int a[] = new int[100];
+		int number = 0;
+		int result = 0;
+		for (int i = 0; i < str.length(); i++) {
+			if (Character.isDigit(str.charAt(i))) {
+				number = Integer.valueOf(str.substring(i, i + 1)).intValue();
+				a[i] = number;
+				result += a[i];
+			}
+		}
+		System.out.println(result);
+	}
+}
